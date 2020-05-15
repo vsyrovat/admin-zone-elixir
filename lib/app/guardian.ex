105f4cba58e11aa86768ebdf4b681570574bf919
VS <v@s>
2020-05-16 01:35:50 +0300

    + Drop sessions on password change

--- lib/app/guardian.ex
+++ lib/app/guardian.ex
@@ -5,10 +5,41 @@ defmodule App.Guardian do
   alias App.Auth.User
 
   @impl Guardian
-  def subject_for_token(user = %User{}, _claims), do: {:ok, "User:#{user.id}"}
+  def subject_for_token(user = %User{}, _claims) do
+    sub = %{id: user.id, passhash: user.password_hash}
+    {:ok, Jason.encode!(sub)}
+  end
+
   def subject_for_token(_, _), do: {:error, "Unknown resource type"}
 
   @impl Guardian
-  def resource_from_claims(%{"sub" => "User:" <> id}), do: {:ok, Repo.get(User, id)}
+  def resource_from_claims(%{"sub" => sub}) do
+    with {:ok, id, passhash} <- extract_sub(sub),
+         {:ok, user} <- get_user(id),
+         :ok <- check_passhash(user, passhash) do
+      {:ok, user}
+    end
+  end
+
   def resource_from_claims(_), do: {:error, "Unknown resource type"}
+
+  defp extract_sub(sub) do
+    case Jason.decode(sub) do
+      {:ok, %{"id" => id, "passhash" => passhash}} -> {:ok, id, passhash}
+      _ -> {:error, "Unknown resource type"}
+    end
+  end
+
+  defp get_user(id) do
+    case Repo.get(User, id) do
+      nil -> {:error, "No such user"}
+      user -> {:ok, user}
+    end
+  end
+
+  defp check_passhash(user, passhash) do
+    if user.password_hash == passhash,
+      do: :ok,
+      else: {:error, "Password invalid"}
+  end
 end
